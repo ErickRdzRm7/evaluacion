9f39f37a4893badb4ad9961a15c595805e3ea3db
erick <erick@ericks-MacBook-Pro.local>
2025-09-22 14:09:17 -0500

    curl in container

--- infra/terraform-erick/modules/ecs_frontend/main.tf
+++ infra/terraform-erick/modules/ecs_frontend/main.tf
@@ -21,7 +21,7 @@ resource "aws_ecs_task_definition" "frontend" {
       ]
 
       healthCheck = {
-        command     = ["CMD-SHELL", "curl -f http://localhost:80/ || exit 1"]
+        command     = ["CMD-SHELL", "nc -z localhost 80 || exit 1"]
         interval    = 30
         timeout     = 5
         retries     = 3
